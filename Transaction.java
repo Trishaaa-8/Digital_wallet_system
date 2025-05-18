4dc024142b242053aebcdceac565261315ee2234
Trishaaa-8 <aec.ece.trishakumari.2024.016@gmail.com>
2025-05-18 11:51:23 +0530

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -14,3 +14,35 @@ public class Transaction {
 
     // Getters, Setters
 }
+//--- Repository Interfaces ---
+public interface UserRepository extends JpaRepository<User, Long> {
+ Optional<User> findByUsername(String username);
+}
+
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+ Optional<Wallet> findByUser(User user);
+}
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+ List<Transaction> findByFromUser(User user);
+ List<Transaction> findByToUser(User user);
+}
+
+//--- Security Config (JWT + BCrypt) ---
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+ @Override
+ protected void configure(HttpSecurity http) throws Exception {
+     http.csrf().disable()
+         .authorizeRequests()
+         .antMatchers("/api/auth/**", "/swagger-ui/**").permitAll()
+         .anyRequest().authenticated();
+ }
+
+ @Bean
+ public PasswordEncoder passwordEncoder() {
+     return new BCryptPasswordEncoder();
+ }
+}
+
